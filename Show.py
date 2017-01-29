82a0d85e2895cc8c4d5ef1f1e16ad2c40443379d
TomekLL <smartalecccc@gmail.com>
2017-01-29 21:59:05 +0100

    Add files via upload

---
+++ Show.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BOARD)
+displeje = (18,22,32,36,38,40)
+GPIO.setup(displeje,GPIO.OUT, initial=1)
+segments = (13,11,35,33,31,15,29,37)
+GPIO.setup(segments,GPIO.OUT, initial=1)
+num = {' ':(1,1,1,1,1,1,1,1),
+       'F':(0,1,1,1,0,0,0,1),
+       'A':(0,0,0,1,0,0,0,1),
+       'C':(0,1,1,0,0,0,1,1),
+       'b':(1,1,0,0,0,0,0,1),
+       'd':(1,0,0,0,0,1,0,1),
+       'E':(0,1,1,0,0,0,0,1),
+       'H':(1,0,0,1,0,0,0,1),
+       'b':(),
+       'n':(1,1,0,1,0,1,0,1),
+       'o':(1,1,0,0,0,1,0,1),
+       'P':(0,0,1,1,0,0,0,1),
+       'r':(1,1,1,1,0,1,0,1),
+       'S':(0,1,0,0,1,0,0,1),
+       'I':(1,1,1,1,0,0,1,1),
+       'L':(1,1,1,0,0,0,1,1),
+       '0':(0,0,0,0,0,0,1,1),
+       'O':(0,0,0,0,0,0,1,1),
+       '1':(1,0,0,1,1,1,1,1),
+       '2':(0,0,1,0,0,1,0,1),
+       '3':(0,0,0,0,1,1,0,1),
+       '4':(1,0,0,1,1,0,0,1),
+       '5':(0,1,0,0,1,0,0,1),
+       '6':(0,1,0,0,0,0,0,1),
+       '7':(0,0,0,1,1,1,1,1),
+       '8':(0,0,0,0,0,0,0,1),
+       '9':(0,0,0,1,1,0,0,1)
+    }
+def seg():
+    for displej in range(4):
+        GPIO.output(segments, (num[display_string[displej]]))
+        GPIO.output(displeje[displej],1)
+        sleep(.005)
+        GPIO.output(displeje[displej],0)
+
+try:
+    while(1):
+      display_string = 'HELL'
+      seg()
+finally:
+    GPIO.cleanup()
