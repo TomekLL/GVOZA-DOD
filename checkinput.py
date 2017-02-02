9ce54da7de7d663a41a1b1baabdcd63c27968c77
Kavec1 <matus.kavec@gmail.com>
2017-02-02 19:02:28 +0100

    Update checkinput.py

--- checkinput.py
+++ checkinput.py
@@ -3,7 +3,9 @@ GPIO.setup(12,GPIO.in,pull_up_down=GPIO.PUD_UP)
 def stlacene_tlacitko():
 	if GPIO.inpu(7)==0:
 		return 1
+		sleep(.2)
 	if GPIO.inpu(7)==0:
 		return 2
+		sleep(.2)
 	return 0
 	#vrati 1 ak je stlacene lave tlacitko, 2 ak pravea 0 ak ziadne
