381f193bc2f3ee1622ac61df4830ee51b6970c05
Kavec1 <matus.kavec@gmail.com>
2017-02-02 21:03:12 +0100

    Update checkinput.py

--- checkinput.py
+++ checkinput.py
@@ -1,10 +1,10 @@
-GPIO.setup(7,GPIO.in,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(12,GPIO.in,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(7,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 def stlacene_tlacitko():
 	if GPIO.inpu(7)==0:
 		return 1
 		sleep(.2)
-	if GPIO.inpu(7)==0:
+	if GPIO.inpu(12)==0:
 		return 2
 		sleep(.2)
 	return 0
